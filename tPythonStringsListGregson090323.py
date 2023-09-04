4b7d8a0367094bfdee423b173250ab1d3804aa8c
MtnManiac <juli.gregson@me.com>
2023-09-03 20:23:43 -0700

    Add files via upload

---
+++ tPythonStringsListGregson090323.py
@@ -0,0 +1,39 @@
+#Juli Gregson - tPython Strings List
+text="Python is an amazing programming language. It is versatile, easy to learn, and powerful"
+      
+# Length calculation
+the_length = len(text)
+      
+print(the_length)
+
+print(text.upper()
+
+
+#Upper case followed by lower case
+Upper = (text.upper())
+    
+Lower = (text.lower())
+      
+print(Upper, Lower)
+
+
+#Word Count
+print(text.count(' '))
+
+
+#Substring extraction
+A = input("Enter a number between two and three")
+B = input("Enter a number between seven and eight")
+sub_string = text [A : B]
+print(sub_string)
+
+#Word Replacement
+C = input("Enter a six-letter noun")
+D = input("Enter a four-letter adjective")
+print(text.replace('Python', 'easy')
+      
+
+
+
+
+
